79ba3df0c244217417e73950015aba84921c1ef8
Aziguy <monsieur_hernandez@hotmail.com>
2023-05-09 13:08:55 +0200

    Try to set good python runtime

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# GDAL SCRIPT INSTALL
-echo "GDAL Install..."
-add-apt-repository ppa:ubuntugis/ppa && apt-get update
-apt-get update
-apt-get install gdal-bin
-apt-get install libgdal-dev
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
-pip install GDAL
-
 # Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
