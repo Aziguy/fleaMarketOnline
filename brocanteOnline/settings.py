26b1f1bd71b13d8f252f9212c8b4c88a750242ac
Aziguy <monsieur_hernandez@hotmail.com>
2023-05-09 09:47:49 +0200

    change python runtime

--- brocanteOnline/settings.py
+++ brocanteOnline/settings.py
@@ -177,3 +177,7 @@ DEFAULT_FROM_EMAIL = "fleaMarketOnline <mandatairess@gmail.com>"
 
 # Google config
 GOOGLE_API_KEY = config("GOOGLE_API_KEY")
+
+# IMPORTANT FOR VERCEL
+GDAL_LIBRARY_PATH = "libgdal.so"
+GEOS_LIBRARY_PATH = "libgeos_c.so.1"
